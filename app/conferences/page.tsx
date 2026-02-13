1972d0e317c0400e42429d4a564ded5cedfe049a
AngelMrtnz <93847033+AngelMrtnz@users.noreply.github.com>
2026-02-13 11:27:06 +0100

    Update page.tsx

--- app/conferences/page.tsx
+++ app/conferences/page.tsx
@@ -104,7 +104,7 @@ export default function Conferences() {
                           {event.description}
                           {event.poster && (
                             <a 
-                              href={event.poster}
+                              href={`${process.env.PUBLIC_URL}${event.poster}`}
                               target="_blank"
                               rel="noopener noreferrer"
                               className="text-urv-red hover:text-comp-red transition-colors"
